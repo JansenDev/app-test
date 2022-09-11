9a3d243fb297c898e01b2cdd48fc1258ebb0176f
JHONATAN JANSSEN SEGURA GALINDO <jsegura-e@ripley.com.pe>
2022-09-11 14:57:21 -0500

    deploy.sh fix

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,35 @@
 #!/usr/bin/env sh
 
 # abort on errors
-set -e
+# set -e
+
+# clean old dist if exists
 
 # build
-npm run build
+npm run predeploy
 
-# navigate into the build output directory
-cd dist
+# navigate into temp directory
+mkdir .temp
+cd .temp
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
+# Get old git commits
 git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
+git remote add origin https://github.com/JansenDev/app-test.git
+git pull origin gh-pages
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# update new changes
+#unalias cp
+cp -fr ../dist/* .
+
+# upload changes
+git add -A
+git commit -m 'deploy #3'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:JanssenDev/app-test.git main:gh-pages
+git push -f https://github.com/JansenDev/app-test.git main:gh-pages
 
-cd -
+cd ../
+rm -drf .temp
+# rm -drf dist
+
+#cd -
